2c5d6492800bb2379734c41143d8dd72a2517b26
Alex <alexgfh@gmail.com>
2021-02-24 17:24:45 -0300

    Add coin info to header, increment Concept text

--- src/Concept.tsx
+++ src/Concept.tsx
@@ -10,11 +10,12 @@ function Burn() {
       {/*<img src={logo} className="App-logo" alt="logo" />*/}
       <div className="Concept">
         <p>
-          Players can emit tokens from your game into the Solana blockchain.
-          <p>
-            You burn 1 token internally for every 1 token emitted, so as to keep
-            the economy sound.
-          </p>
+          Players can emit tokens from outside your game, into the Solana
+          blockchain.
+        </p>
+        <p>
+          You burn 1 token internally for every 1 token emitted, so as to keep
+          the economy sound.
         </p>
         <p>
           Burn tokens as you spawn creatures or any other form of loot
@@ -27,6 +28,11 @@ function Burn() {
         <p>
           To feel the state of your economics, much like the head of a central
           bank would manage their country's money supply.
+        </p>
+        <p>
+          More strict strategies can be employed such as limiting the spawning
+          of creatures in-game, i.e.: if a creature yields 10 tokens. Only spawn
+          it for every 10 tokens burned in-server.
         </p>
       </div>
     </div>
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import "./NavBar.css";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import tokenClient from "./browserclient.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-interface Coin {
-  Owner: string;
-  Mint: string;
-}
-
 function NavBar() {
+  let [supply, setSupply] = useState(undefined);
+  let [name, setName] = useState("");
   let history = useHistory();
   let storage = localStorage.getItem("coin");
+  let coin = storage ? JSON.parse(storage) : null;
+  useEffect(() => {
+    tokenClient.coinInfo(coin.Mint).then((info) => {
+      setSupply(info.Supply);
+      setName("My Coin Name");
+    });
+  });
+
   return (
     <div>
-      {1 ? (
-        <div className="Coin Info">
-          CoinName:..., Owner: ..., MintAddress:..., Supply:...{" "}
+      {coin ? (
+        <div className="CoinInfo">
+          <p>CoinName: {name ?? "Loading..."}</p>
+          <p>Owner: {coin.Owner}</p>
+          <p>MintAddress: {coin.Mint}</p>
+          <p>Supply: {supply ?? "Loading..."}</p>
         </div>
       ) : (
         <></>
